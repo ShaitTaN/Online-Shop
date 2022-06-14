03d26973f03ca22ca2282c9473fd8807aa6f8132
Dmitriy <kdima18@mail.ru>
2022-06-14 17:34:17 +0300

    added product counter on catalog card

--- src/components/Catalog/CatalogItem.tsx
+++ src/components/Catalog/CatalogItem.tsx
@@ -1,13 +1,26 @@
 import React, { FC } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { IProduct } from "../../models/IProduct";
+import { cartSlice } from "../../store/reducers/Cart";
 
-const CatalogItem:FC<IProduct> = ({price, image, title}) => {
+const CatalogItem:FC<IProduct> = (props) => {
+	const {addToCart} = cartSlice.actions
+	const dispatch = useAppDispatch()
+	const {products} = useAppSelector(state => state.cart)
+
+	const add = (product: IProduct) => {
+		dispatch(addToCart(product))
+	}
+
+	const count = products.filter(item => item.id === props.id).length
+
   return (
     <div className="catalog__cards-item">
-      <span className="text-right text-green-800">{price} $</span>
-      <img className="h-[200px]" src={image} />
-      <p className="text-center uppercase my-4">{title.substring(0, 20) + '...'}</p>
-      <div className="catalog__cards-btn">В корзину</div>
+			{!!count && <div className="absolute right-4 top-2 flex justify-center items-center text-xs w-[20px] h-[20px] rounded-full bg-red-400">{count}</div>}
+      <span className="text-right text-green-800">{props.price} $</span>
+      <img className="h-[200px]" src={props.image} />
+      <p className="text-center uppercase my-4">{props.title.substring(0, 20) + '...'}</p>
+      <div onClick={() => add(props)} className="catalog__cards-btn">В корзину</div>
     </div>
   );
 };
--- src/store/reducers/Cart.ts
+++ src/store/reducers/Cart.ts
@@ -1,19 +1,19 @@
 import { IProduct } from './../../models/IProduct';
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState:IProduct[] = []
+const initialState = {
+	products: [] as IProduct[]
+}
 
 export const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
 	reducers: {
 		addToCart(state, action: PayloadAction<IProduct>) {
-			state.push(action.payload)
+			state.products.push(action.payload)
 		},
 		removeFromCart(state, action: PayloadAction<number>) {
-			state = state.filter(item => item.id !== action.payload)
+			state.products = state.products.filter(item => item.id !== action.payload)
 		}
 	}
 })
-
-export const {} = cartSlice
